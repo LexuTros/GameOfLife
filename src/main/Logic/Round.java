2ea6a355e2c31680f9aed3c08f31417487c926c4
LexuTros <lennart@toellke.com>
2022-12-24 17:10:50 +0100

    refactoring of programm structure

---
+++ src/main/Logic/Round.java
@@ -0,0 +1,25 @@
+package Logic;
+
+import DataStructure.Cell;
+
+public class Round {
+    private void determineCellFuture(Cell cell){
+
+    }
+
+    private void progressCell(Cell cell){
+
+    }
+
+    private void determineGridFuture(int rows, int cols){
+
+    }
+
+    private void progressGrid(int rows, int cols){
+
+    }
+
+    public void nextGeneration(int rows, int cols){
+
+    }
+}
