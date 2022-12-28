07c82ac28080bdc73bd62e3920d1779aeae09e31
wigeto <remo.wiget@gmail.com>
2022-12-28 17:00:09 +0100

    Beautify Gui, create GuiWinner

--- src/main/InterfaceHelpers/LabelPlayerName.java
+++ src/main/InterfaceHelpers/LabelPlayerName.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class LabelPlayerName extends JLabel {
     public LabelPlayerName() {
         this.setBounds(10, 10, 120, 30);
-        this.setForeground(Color.BLUE);
+        this.setForeground(Color.BLACK);
         this.setFont(new Font("MV Boli", Font.BOLD, 20));
         this.setOpaque(true);
         this.setVisible(true);
