d573d6011b4b597f54edfe153f8c881b676473c3
dariomonopoli-dev <jerryscout71@gmail.com>
2022-12-29 14:37:41 +0100

    removed game class and player + cell tests

--- src/test/DataStructure/PlayerTest.java
+++ src/test/DataStructure/PlayerTest.java
@@ -4,10 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 public class PlayerTest {
     @Test
     public void testGetPlayerName() {
-        Player p = new Player("Philipp", "blue");
+        Player p = new Player("Philipp", Color.GREEN);
         assertEquals("Philipp", p.getPlayerName());
         assertTrue(p.getPlayerName().length() > 0);
         assertTrue(p.getPlayerName().length() <= 18);
@@ -17,26 +19,26 @@ public class PlayerTest {
 
     @Test
     public void testGetPlayerColor() {
-        Player p = new Player("Johnny", "green");
+        Player p = new Player("Johnny", Color.GREEN);
         assertEquals(Color.GREEN, p.getPlayerColor());
     }
 
     @Test
     public void testGetPlayerAliveCells() {
-        Player p = new Player("Mark", "red");
+        Player p = new Player("Mark", Color.RED);
         assertEquals(0, p.getAliveCells());
     }
 
     @Test
     public void testGetAliveCells() {
-        Player p = new Player("Carl", "orange");
+        Player p = new Player("Carl", Color.ORANGE);
         p.setAliveCells(5);
         assertEquals(5, p.getAliveCells());
     }
 
     @Test
     public void testGetIsAlive() {
-        Player p = new Player("Chris", "gray");
+        Player p = new Player("Chris", Color.GRAY);
         p.setAliveCells(5);
         assertTrue(p.isAlive());
         p.setAliveCells(-5);
